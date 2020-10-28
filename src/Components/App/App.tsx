c08892ec7a877d9e278ec0e519acefbdfe9b1c19
Michał Prawda <m.prawda99@gmail.com>
2020-10-28 16:23:14 +0100

    Add navbar and theme, distribute files
    Added vscode folders to ignore in gitignore
    Added div for navbar in index
    Moved app to Components/App/
    Added navbar component
    Added theme for the wholle app

---
+++ src/Components/App/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Button} from '@material-ui/core'
+import './App.css';
+
+function App() {
+  return (
+    <Button variant="contained">Button</Button>
+  );
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
+import App from './Components/App/App';
+import Navbar from './Components/Navbar/Nabvar';
+import theme from './theme';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <ThemeProvider theme={theme}>
+    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+    <CssBaseline />
+    <Navbar />
+  </ThemeProvider>,
+  document.getElementById('navbar'),
 );
 
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+    <CssBaseline />
+    <App />
+  </ThemeProvider>,
+  document.getElementById('root'),
+);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
