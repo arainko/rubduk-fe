6f76503086346dff2072cbe6cd832f0c3396b687
Aleksander Rainko <aleksander.rainko99@gmail.com>
2020-11-23 16:28:50 +0100

    added GoogleAuthButton.tsx

---
+++ src/Components/GoogleAuthButton/GoogleAuthButton.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GoogleLogin from 'react-google-login';
+
+const responseGoogle = (response: any) => {
+    console.log(response.tokenId);
+}
+
+const GoogleAuthButton = (): JSX.Element => {
+    return (
+        <GoogleLogin
+            clientId="473547529565-sk3f13t2p7tn2rfc56mbqf4hkqbt80ub.apps.googleusercontent.com"
+            buttonText="Login"
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+            cookiePolicy={'single_host_origin'}
+        />
+    );
+};
+
+export default GoogleAuthButton;
