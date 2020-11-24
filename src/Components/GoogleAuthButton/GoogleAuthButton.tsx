4c14039e23783367bbad756df7f1e220a67f35fe
Michał Prawda <m.prawda99@gmail.com>
2020-11-24 21:04:25 +0100

    Basic working state change

--- src/Components/GoogleAuthButton/GoogleAuthButton.tsx
+++ src/Components/GoogleAuthButton/GoogleAuthButton.tsx
@@ -1,21 +1,33 @@
 import React from 'react';
-import {useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
 import GoogleLogin from 'react-google-login';
+import { logIn }  from '../Redux/Actions';
 
-const responseGoogle = (response: any) => {
-    console.log(response.tokenId);
+interface RootState {
+    isLogged: boolean
 }
 
-const GoogleAuthButton = (): JSX.Element => {
+export default function GoogleAuthButton() {
+
+    const responseGoogle = (response: any) => {
+        console.log(response.tokenId);
+    }
+    
+    const isLogged = useSelector((state: RootState) => state.isLogged);
+    
+    const dispatch = useDispatch();
+    
     return (
-        <GoogleLogin
-            clientId="473547529565-sk3f13t2p7tn2rfc56mbqf4hkqbt80ub.apps.googleusercontent.com"
-            buttonText="Login"
-            onSuccess={responseGoogle}
-            onFailure={responseGoogle}
-            cookiePolicy={'single_host_origin'}
-        />
+        <div>
+            <GoogleLogin
+                clientId="473547529565-sk3f13t2p7tn2rfc56mbqf4hkqbt80ub.apps.googleusercontent.com"
+                buttonText="Login"
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+                cookiePolicy={'single_host_origin'}
+            />
+            <button onClick={() => dispatch(logIn())}></button>
+            {isLogged ? <h3>You are now logged in</h3> : <h3>Click to log in</h3>}
+        </div>
     );
 };
-
-export default GoogleAuthButton;
