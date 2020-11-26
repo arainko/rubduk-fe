ee15bac24d242b5cd899bc44198eeb84a095283c
Stefan Popiołek <studentstefan@wp.pl>
2020-11-26 18:49:08 +0100

    Comment

---
+++ src/Components/CommentsModal/Comment.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import theme from '../../theme';
+import FaceIcon from '@material-ui/icons/Face'
+import {Paper} from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
+import Post from "../Post/Post";
+
+const useStyles = makeStyles({
+    comment: {
+        backgroundColor: theme.palette.primary.light,
+        color: theme.palette.primary.dark,
+        overflowWrap: "break-word"
+    },
+    commentOwner: {
+        color: theme.palette.secondary.dark,
+        paddingLeft:5,
+    },
+    whole: {
+        backgroundColor: theme.palette.primary.light,
+        margin:5,
+    }
+});
+interface CommentProps {
+    userId: number,
+    userName: string,
+    userLastName: string,
+    contents: string,
+    dateAdded: Date
+}
+const Comment = (props: CommentProps) => {
+    const classes = useStyles();
+    return (
+        <Paper className={classes.whole} text-align="justify">
+            <p className={classes.commentOwner}>
+                <FaceIcon/>{props.userName} {props.userLastName}
+                <span title="date"> {props.dateAdded}</span>
+            </p>
+            <Typography className={classes.comment} >{props.contents}</Typography>
+        </Paper>
+
+    )
+
+}
+export default Comment;
--- src/Components/Pages/LoginPage/LoginPage.tsx
+++ src/Components/Pages/LoginPage/LoginPage.tsx
@@ -1,63 +1,15 @@
-import {makeStyles, Theme} from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider } from "@material-ui/core/styles";
 import React from "react";
+import theme from "../../../theme";
 import GoogleAuthButton from "../../GoogleAuthButton/GoogleAuthButton";
-import Typography from "@material-ui/core/Typography";
-import Link from "@material-ui/core/Link";
-import Grid from "@material-ui/core/Grid"
-import {Paper} from "@material-ui/core";
-
-const useStyles = makeStyles((theme:Theme) => ({
-    link: {
-        underline: 'none',
-        color: 'inherit'
-    },
-    title: {
-        flexGrow: 1,
-        color:'secondary',
-    },
-    root:{
-        flexGrow:1,
-    },
-    item:{
-        overflowWrap: "break-word",
-    },
-    view:{
-        width:'60vh',
-        paddingLeft:'20vh',
-    },
-    typo:{
-      align:"center",
-      color:theme.palette.primary.contrastText,
-    },
-}));
 
 const LoginPage = () => {
-    const classes = useStyles();
     return (
-        <div className={classes.root}>
-                    <Typography color="secondary" variant="h1" >
-                        <Link href="/" underline="none" className={classes.link}>
-                            {'RubDuk'}
-                        </Link>
-                    </Typography>
-            <Grid container spacing={2} className={classes.root} xs={12} justify="space-evenly">
-                        <Grid item className={classes.view} alignContent="center">
-                            <Paper className={classes.item}>
-                                <Typography variant="h3" className={classes.typo}>Connecting People since 2020 maybe somthing intresting and blablablablablablablablablablablabla</Typography>
-                            </Paper>
-                        </Grid>
-                         <Grid item className={classes.view}>
-                             <Paper className={classes.item}>
-                                 <Typography align="center" variant="h3" color="secondary">Welcome</Typography>
-                                 <Typography variant="h4" className={classes.typo}>Please log in with Google to continue</Typography>
-                                 <Typography className={classes.typo}>
-                                     <GoogleAuthButton/>
-                                 </Typography>
-                             </Paper>
-                        </Grid>
-
-            </Grid>
-        </div>
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <GoogleAuthButton/>
+        </ThemeProvider>
     )
 }
 
